02df885ded31868c3ab5f897063fd5a5b01b5a2e
Nitschke, Daniel <nitschke.daniel@volkswagen.de>
2022-02-23 13:36:45 +0100

    Initializr

---
+++ src/main/java/de/volkswagen/f73/galvanize/AutomobileCrudApplication.java
@@ -0,0 +1,13 @@
+package de.volkswagen.f73.galvanize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomobileCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomobileCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/volkswagen/f73/galvanize/AutomobileCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package de.volkswagen.f73.galvanize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomobileCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
