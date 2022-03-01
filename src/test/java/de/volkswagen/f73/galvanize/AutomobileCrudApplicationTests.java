a635d5129e97e02ed48b4ba5a722e52c2889dc65
Nitschke, Daniel <nitschke.daniel@volkswagen.de>
2022-03-01 12:15:11 +0100

    test getAutos_searchByColorAndMake_returnsAutosList

--- src/test/java/de/volkswagen/f73/galvanize/AutomobileCrudApplicationTests.java
+++ src/test/java/de/volkswagen/f73/galvanize/AutomobileCrudApplicationTests.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -82,5 +83,26 @@ class AutomobileCrudApplicationTests {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isNotNull();
         assertThat(response.getBody()).isEqualTo(expectedCar);
+    }
+
+    @Test
+    void getAutos_searchByColorAndMake_returnsAutosList() {
+        int randomCarIndex = rand.nextInt(NUM_AUTOMOBILES);
+        Automobile primeAuto = this.autosList.get(randomCarIndex);
+
+        List<Automobile> expectedAutos = this.autosList.stream()
+                .filter(auto -> auto.getMake().equals(primeAuto.getMake()))
+                .filter(auto -> auto.getColor().equals(primeAuto.getColor()))
+                .collect(Collectors.toList());
+
+        ResponseEntity<AutosList> response = restTemplate.getForEntity(
+                String.format("/api/autos?color=%s&make=%s", primeAuto.getColor(), primeAuto.getMake()),
+                AutosList.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().isEmpty()).isFalse();
+        assertThat(response.getBody().getAutomobiles()).isNotNull();
+        assertThat(response.getBody().getAutomobiles().size()).isEqualTo(expectedAutos.size());
     }
 }
